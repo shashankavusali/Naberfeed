76812a8145f55002db78d320f0f277da2529758f
Thitivuth Rattanasriampaipong <Thitivuth@Thitivuths-MacBook-Pro.local>
2017-01-22 20:59:12 -0600

    update hometownnews with too many links

--- hometownnews/hometownnews.py
+++ hometownnews/hometownnews.py
@@ -1,44 +1,55 @@
+# hometownnews.py
 import urllib2
 import csv
 from bs4 import BeautifulSoup
 
-# hometownnews = "http://www.hometownnews.com/"
-hometownnews = 'http://www.hometownnews.com/paper/show/B61FCB60A3774105'
+hometownnews = "http://www.hometownnews.com/"
+
 page = urllib2.urlopen(hometownnews)
 
-print(page.geturl())
 soup = BeautifulSoup(page, "html.parser")
 
 dropdown = soup.find('select')
 
-# f = open('myfile.txt','w')
-
+state = []
 name = []
 URL  = []
 
 for tag in dropdown.find_all('option', selected=False):
-	# print(tag['value'])
-	# print(tag.get_text())
-	statepage = urllib2.urlopen(tag['value'])
-	statesoup = BeautifulSoup(statepage, "html.parser")
+		statepage = urllib2.urlopen(tag['value'])
+		statesoup = BeautifulSoup(statepage, "html.parser")
 
-	header = statesoup.find('h3')
-	current_state = header.next
+		header = statesoup.find('h3')
+		current_state = header.next
 
-	table = statesoup.find('table')
+		table = statesoup.find('table')
 
-	for cell in table.find_all('td'):
-		print(cell['data-link'])
-		print(cell.get_text().strip())
-		name.append(cell.get_text().strip())
-		URL.append(cell['data-link'])
+		for cell in table.find_all('td'):
+			# print(cell['data-link'])
+			# print(cell.get_text().strip())
+			newsPage = urllib2.urlopen(cell['data-link'])
+			if newsPage.geturl() != "http://www.hometownnews.com/home/default":	
+				state.append(current_state)
+				name.append(cell.get_text().strip())
+				newsSoup = BeautifulSoup(newsPage, "html.parser")
+				link = newsSoup.find('a', target='_blank')
+				URL.append(link['href'])
+				# print(link['href'])
+				# print("\n")
 
-		with open(current_state + '.csv','w') as csvfile:
-			writer_0 = csv.writer(csvfile,delimiter=',')
-			for i in range(len(name)):
-				writer_0.writerow([current_state,name[i],URL[i]])
-		# f.write(cell['data-link'])
-		# f.write(cell.get_text().strip())
-		# f.write('\n')
+################################################
+#BY STATE FILE
+################################################
+			# with open ('./by_state/' + current_state + '.csv','w') as csvfile:
+			# # with open(current_state + '.csv','w') as csvfile:
+			# 	writer_0 = csv.writer(csvfile,delimiter=',')
+			# 	for i in range(len(name)):
+			# 		writer_0.writerow([current_state,name[i],URL[i]])
 
-# f.close()
+################################################
+#SINGLE FILE 'allstates.csv'
+################################################
+with open('all.csv','w') as csvfile:
+	writer_0 = csv.writer(csvfile,delimiter=',')
+	for i in range(len(name)):
+		writer_0.writerow([state[i],name[i],URL[i]])
