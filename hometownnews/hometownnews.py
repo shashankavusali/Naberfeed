c17a29c1ba03d54af8d07ac28f179ea9c5e2f9e4
Carlos Martinez Villar <cmv@Carloss-MBP.PK5001Z>
2017-01-30 00:54:53 -0600

    added rename.py, fixed state name to acronym in hometownnews site.

--- hometownnews/hometownnews.py
+++ hometownnews/hometownnews.py
@@ -4,12 +4,27 @@ import csv
 from bs4 import BeautifulSoup
 
 hometownnews = "http://www.hometownnews.com/"
-
 page = urllib2.urlopen(hometownnews)
-
 soup = BeautifulSoup(page, "html.parser")
-
 dropdown = soup.find('select')
+
+state_acronyms = ["AL","AK","AZ","AR","CA","CO","CT",
+	"DE","DC","FL","GA","HI","ID","IL","IN","IA",
+	"KS","KY","LA","ME","MD","MA","MI","MN","MS",
+	"MO","MT","NE","NV","NH","NJ","NM","NY","NC",
+	"ND","OH","OK","OR","PA","RI","SC","SD","TN",
+	"TX","UT","VT","VA","WA","WV","WI","WY"]
+
+states_full = [
+"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut",
+"Delaware","District of Columbia","Florida","Georgia","Hawaii","Idaho","Illinois",
+"Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
+"Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+"Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
+"North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
+"Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+"Vermont","Virgina","Washington","West Virginia","Wisconsin","Wyoming"
+]
 
 state = []
 name = []
@@ -20,7 +35,8 @@ for tag in dropdown.find_all('option', selected=False):
 		statesoup = BeautifulSoup(statepage, "html.parser")
 
 		header = statesoup.find('h3')
-		current_state = header.next
+		aState = header.next
+		current_state = state_acronyms[states_full.index(str(aState).strip())]
 
 		table = statesoup.find('table')
 
@@ -28,7 +44,7 @@ for tag in dropdown.find_all('option', selected=False):
 			# print(cell['data-link'])
 			# print(cell.get_text().strip())
 			newsPage = urllib2.urlopen(cell['data-link'])
-			if newsPage.geturl() != "http://www.hometownnews.com/home/default":	
+			if newsPage.geturl() != "http://www.hometownnews.com/home/default":
 				state.append(current_state)
 				name.append(cell.get_text().strip())
 				newsSoup = BeautifulSoup(newsPage, "html.parser")
